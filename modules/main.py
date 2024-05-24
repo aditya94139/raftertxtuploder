492423f036be5d0993e90c1d9568e9cd2961d090
aditya94139 <152467027+aditya94139@users.noreply.github.com>
2024-05-24 18:14:16 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -30,7 +30,7 @@ bot = Client("bot",
 
 @bot.on_message(filters.command(["start"]))
 async def account_login(bot: Client, m: Message):
-    editable = await m.reply_text("𝙃𝙄𝙄 ┈━═𝙈𝙮 𝙁𝙧𝙞𝙚𝙣𝙙═━┈😎\n\n I Am A Bot For Download Links From Your .TXT File And Then Upload That File Om Telegram So Basically If You Want To Use Me First Send Me Hidden Command And Then Follow Few Steps..r@j❤️")
+    editable = await m.reply_text("𝙃𝙄𝙄 ┈━═𝙈𝙮 𝙁𝙧𝙞𝙚𝙣𝙙═━┈😎\n\n I Am A Bot For Download Links From Your .TXT File And Then Upload That File Om Telegram So Basically If You Want To Use Me First Send Me Hidden Command And Then Follow Few Steps")
 
 
 @bot.on_message(filters.command("stop"))
@@ -101,7 +101,7 @@ async def account_login(bot: Client, m: Message):
     
     
 
-    await editable.edit("Now Enter A Caption to add caption on your uploaded file\n ex: @sccwaleyarr")
+    await editable.edit("Now Enter A Caption to add caption on your uploaded file\n ex: @maisamyahu")
     input3: Message = await bot.listen(editable.chat.id)
     raw_text3 = input3.text
     await input3.delete(True)
